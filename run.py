9927259f77b3d6da132ae970b66636e8ac1daaf4
El Mehdi Rami <lbiggboss19@gmail.com>
2021-03-15 03:10:59 +0100

    implemented custom commands

--- run.py
+++ run.py
@@ -1,4 +1,28 @@
-from app import app
+import click
+from app import app, db
+
+
+@click.command(name='create-db')
+def create_db():
+    """Create database tables"""
+    from app.models import User
+    db.create_all()
+    click.echo('Tables created')
+
+
+app.cli.add_command(create_db)
+
+
+@click.command(name='clear-db')
+def clear_db():
+    """Drop database tables"""
+    from app.models import User
+    db.drop_all()
+    click.echo('Database cleared')
+
+
+app.cli.add_command(clear_db)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
